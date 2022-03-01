a8b9aa081a9d145d63b2e34cd1997df7b2ec02c9
PhilipPWD <98983567+PhilipPWD@users.noreply.github.com>
2022-03-01 15:31:01 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mfMvRkVXul4fVmL_-kWoK4koLLboHPhp8UFZUvDuDao/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCwki7o7irju_zw9Gb8ZwCv8CiKJdSys70';
